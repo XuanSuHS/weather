25aba483608a06079f9cc4e1988e7eed7e79cd09
XuanSuHS <XuanSuHSia@outlook.com>
2023-04-12 21:08:44 +0800

    增加重置Cookie及重载设置命令

---
+++ src/main/kotlin/Coroutine.kt
@@ -0,0 +1,11 @@
+package top.xuansu.mirai.weather
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+fun reloadCookie() {
+    CoroutineScope(Dispatchers.IO).launch {
+        Web.getCookie()
+    }
+}

fd8650d2c5b7cde84b93353b172286de5d554e21
XuanSu <XuanSuHSia@outlook.com>
2023-05-23 23:49:15 +0800

    增加不使用代理的功能

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -5,6 +5,7 @@ import net.mamoe.mirai.console.data.value
 
 object Config : AutoSavePluginConfig("config") {
     val commands: List<String> by value(listOf("未来天气", "天气如何"))
+    var isProxyEnabled: Boolean by value(false)
     var proxyAddress: String by value("127.0.0.1:7890")
     val enableGroups: MutableSet<Long> by value(mutableSetOf())
 }

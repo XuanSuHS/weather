fd2861b5799bb520f4de5a977160aab1b421e5cd
XuanSu <XuanSuHSia@outlook.com>
2023-05-25 22:47:27 +0800

    Bump Version to v0.1.3-B10

--- src/main/kotlin/Weather.kt
+++ src/main/kotlin/Weather.kt
@@ -12,7 +12,7 @@ object weatherMain : KotlinPlugin(
     JvmPluginDescription(
         id = "top.xuansu.mirai.weather",
         name = "Weather",
-        version = "0.1.3-B9",
+        version = "0.1.3-B10",
     ) {
         author("XuanSu")
     }
